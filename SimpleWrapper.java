188ce3e83e7ce6b3b4b076be0a48814162d4ce42
DhanushaDudekula <dhanusha5689@gmail.com>
2025-06-02 22:01:48 +0530

    Add files via upload

---
+++ SimpleWrapper.java
@@ -0,0 +1,15 @@
+package hello;
+
+public class SimpleWrapper {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=1;
+		Integer b=1;
+		System.out.println(a);
+		System.out.println(b);
+	}
+
+}
+
+
