425ca80526a6efaa1695e733a9fcc95c0a820c8c
DhanushaDudekula <dhanusha5689@gmail.com>
2025-06-08 20:11:51 +0530

    Update TreeMapExample.java

--- TreeMapExample.java
+++ TreeMapExample.java
@@ -4,9 +4,9 @@ import java.util.TreeMap;
 public class TreeMapExample {
     public static void main(String[] args) {
         TreeMap<String, Integer> marks = new TreeMap<>();
-        marks.put("Alice", 85);
-        marks.put("Bob", 78);
-        marks.put("Charlie", 92);
+        marks.put("Afreed", 18);
+        marks.put("Bhaya", 17);
+        marks.put("Chaithu", 19);
 
         System.out.println("Student Marks:");
         for (String name : marks.keySet()) {
@@ -21,8 +21,8 @@ public class TreeMapExample {
         }
 
         TreeMap<String, String> phoneBook = new TreeMap<>();
-        phoneBook.put("Alice", "9876543210");
-        phoneBook.put("Bob", "9123456789");
+        phoneBook.put("Afreed", "9876543210");
+        phoneBook.put("Bhagya", "9123456789");
 
         System.out.println("Phone Book:");
         for (String name : phoneBook.keySet()) {
