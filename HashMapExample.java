907fef9a1c5251a127bccc420e9df806ceef020f
DhanushaDudekula <dhanusha5689@gmail.com>
2025-06-08 19:55:59 +0530

    Update HashMapExample.java

--- HashMapExample.java
+++ HashMapExample.java
@@ -5,9 +5,9 @@ public class HashMapExample {
     public static void main(String[] args) {
         HashMap<String, Integer> scores = new HashMap<>();
 
-        scores.put("Alice", 90);
-        scores.put("Bob", 85);
-        scores.put("Charlie", 92);
+        scores.put("dhanu", 20);
+        scores.put("roja",40);
+        scores.put("giri", 45);
 
         System.out.println(scores);
     }
