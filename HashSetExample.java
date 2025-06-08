395d801a24828f3f9f8468dadaac999c7fd2877b
DhanushaDudekula <dhanusha5689@gmail.com>
2025-06-08 20:00:23 +0530

    Update HashSetExample.java

--- HashSetExample.java
+++ HashSetExample.java
@@ -5,10 +5,10 @@ public class HashSetExample {
     public static void main(String[] args) {
         HashSet<String> colors = new HashSet<>();
 
-        colors.add("Red");
-        colors.add("Green");
-        colors.add("Blue");
-        colors.add("Red"); // Duplicate, will be ignored
+        colors.add("yellow");
+        colors.add("Gray");
+        colors.add("Black");
+        colors.add("Black"); // Duplicate, will be ignored
 
         System.out.println(colors);
     }
