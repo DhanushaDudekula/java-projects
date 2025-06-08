ee006ef541f2dab53384776d1ce832b0f95c9d2d
DhanushaDudekula <dhanusha5689@gmail.com>
2025-06-08 20:05:55 +0530

    Update OverloadExample.java

--- OverloadExample.java
+++ OverloadExample.java
@@ -17,9 +17,9 @@ public class OverloadExample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Calculator c=new Calculator();
-		System.out.println(c.add(1, 2));
+		System.out.println(c.add(13, 20));
 		System.out.println(c.add(1.5, 2.5));
-		System.out.println(c.add(1,2,3));
+		System.out.println(c.add(11,22,33));
 
 	}
 
